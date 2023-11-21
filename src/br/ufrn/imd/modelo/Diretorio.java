75b52ddce8a979b58fdfd0519a1fc19ce1978a2f
glooooh <glorinhamsouza@gmail.com>
2023-11-20 21:06:18 -0300

    Classes bases

---
+++ src/br/ufrn/imd/modelo/Diretorio.java
@@ -0,0 +1,10 @@
+package br.ufrn.imd.modelo;
+
+import java.util.ArrayList;
+
+public class Diretorio {
+    private String caminho;
+    private ArrayList<Musica> musicas;
+    
+    
+}
--- src/br/ufrn/imd/modelo/Playlist.java
+++ src/br/ufrn/imd/modelo/Playlist.java
@@ -1,5 +1,28 @@
 package br.ufrn.imd.modelo;
 
+import java.util.ArrayList;
+
 public class Playlist {
-    
+    private String nome;
+    private ArrayList<Musica> musicas;
+
+    Playlist() {
+        musicas = new ArrayList();
+    }
+
+    public ArrayList<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setMusicas(ArrayList<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
