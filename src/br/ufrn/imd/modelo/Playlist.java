e1b38d80ce3a2874a527f571aa2d44020912481f
glooooh <glorinhamsouza@gmail.com>
2023-11-28 14:14:51 -0300

    UPDATE classe playlist

--- src/br/ufrn/imd/modelo/Playlist.java
+++ src/br/ufrn/imd/modelo/Playlist.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Playlist {
     private String nome;
+    private UsuarioComum usuario;
     private ArrayList<Musica> musicas;
 
-    public Playlist() {
-        musicas = new ArrayList<>();
+    public Playlist(String nome, UsuarioComum usuario) {
+        this.nome = nome;
+        this.usuario = usuario;
+        this.musicas = new ArrayList<>();
     }
 
     public ArrayList<Musica> getMusicas() {
@@ -18,11 +21,19 @@ public class Playlist {
         return nome;
     }
 
+    public UsuarioComum getUsuario() {
+        return usuario;
+    }
+
     public void setMusicas(ArrayList<Musica> musicas) {
         this.musicas = musicas;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setUsuario(UsuarioComum usuario) {
+        this.usuario = usuario;
     }
 }
