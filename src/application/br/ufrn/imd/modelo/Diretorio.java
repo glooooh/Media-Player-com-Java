385188863fdf84a3405b4d8590b943c999f1ff1c
glooooh <glorinhamsouza@gmail.com>
2023-12-06 09:14:19 -0300

    Funções da tela principal

--- src/application/br/ufrn/imd/modelo/Diretorio.java
+++ src/application/br/ufrn/imd/modelo/Diretorio.java
@@ -1,14 +1,25 @@
 package application.br.ufrn.imd.modelo;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Diretorio {
     private String caminho;
     private ArrayList<Musica> musicas;
-    
+
     public Diretorio(String caminho) {
         this.caminho = caminho;
         musicas = new ArrayList<>();
+
+        File diretorio = new File(caminho);
+        File[] files = diretorio.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                musicas.add(new Musica(file.getName(), "", file.toString()));
+                // Artista????
+            }
+        }
     }
 
     public String getCaminho() {
