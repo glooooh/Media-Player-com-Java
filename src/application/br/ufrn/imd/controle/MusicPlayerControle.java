c32947ad7a276f60ea1d0eae70afbe855d561a6b
Marcos Vinicius Noberto de Oliveira <112330160+marcosnoberto@users.noreply.github.com>
2023-12-06 09:03:32 -0300

    Erro de commit

--- src/application/br/ufrn/imd/controle/MusicPlayerControle.java
+++ src/application/br/ufrn/imd/controle/MusicPlayerControle.java
@@ -1,84 +1,209 @@
 package application.br.ufrn.imd.controle;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import application.br.ufrn.imd.modelo.Diretorio;
+import application.br.ufrn.imd.modelo.Musica;
+import application.br.ufrn.imd.modelo.Musica;
+import application.br.ufrn.imd.modelo.Diretorio;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javafx.util.Duration;
 
 public class MusicPlayerControle {
 
 	@FXML
-	private MenuBar menuBar;
+	private Button botaoAdicionarDiretorio;
 
 	@FXML
-	private MenuItem alterarNomeMenuItem;
+	private Button botaoAdicionarMusica;
 
 	@FXML
-	private MenuItem alterarSenhaMenuItem;
+	private Button botaoAdicionarPlaylist;
 
 	@FXML
-	private MenuItem alterarAssinaturaMenuItem;
+	private Button botaoPassarMusica;
 
 	@FXML
-	private MenuItem sairMenuItem;
+	private Button botaoPlay;
 
 	@FXML
-	private TitledPane diretoriosTitledPane;
+	private Button botaoVoltarMusica;
 
 	@FXML
-	private TitledPane playlistsTitledPane;
+	private ListView<?> listaMusicasPlaylist;
 
 	@FXML
-	private TitledPane musicasTitledPane;
+	private ListView<?> listaPlaylists;
 
 	@FXML
-	private Button adicionarMusicaButton;
+	private ListView<String> listaNomesMusicas;
 
 	@FXML
-	private Button adicionarDiretorioButton;
+	private ProgressBar progressoMusica;
 
 	@FXML
-	private Button adicionarPlaylistButton;
+	private Label nomeUsuario;
 
-	@FXML
-	private void initialize() {
-		// Ações de inicialização, se necessário
+	private GerenciadorControle controller;
+
+	private Media media;
+	private MediaPlayer mediaPlayer;
+	private boolean runing;
+	private int musicaSelecionada;
+
+	public void setGerenciadorControle(GerenciadorControle controller) {
+		this.controller = controller;
 	}
 
 	@FXML
-	private void handleAlterarNome(ActionEvent event) {
-		// Lógica para manipular o evento do MenuItem 'Alterar Nome'
+	private void initialize(GerenciadorControle controller) {
+
+	}
+
+	private void diretorioAdicionado() {
+		listaNomesMusicas.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				musicaSelecionada = listaNomesMusicas.getSelectionModel().getSelectedIndex();
+				controller.getListaMusicas().get(musicaSelecionada);
+				tocarMusica(controller.getListaMusicas().get(musicaSelecionada).getCaminho());
+			}
+
+		});
+	}
+
+	private void tocarMusica(String caminho) {
+		File arquivoMusica = new File(caminho);
+
+		if (runing) {
+			this.mediaPlayer.stop();
+		}
+
+		if (arquivoMusica != null) {
+			media = new Media(arquivoMusica.toURI().toString());
+			this.mediaPlayer = new MediaPlayer(media);
+			this.mediaPlayer.play();
+			this.runing = true;
+			carregarProgresso();
+			// Lógica para reproduzir a música
+		}
+	}
+
+	public void setNomeUsuario() {
+		nomeUsuario.setText(controller.getUsuarioLogado().getNome());
 	}
 
 	@FXML
-	private void handleAlterarSenha(ActionEvent event) {
-		// Lógica para manipular o evento do MenuItem 'Alterar Senha'
+	void adicionarDiretorio(ActionEvent event) {
+
+		Stage primaryStage = new Stage();
+		DirectoryChooser selecionarPasta = new DirectoryChooser();
+		selecionarPasta.setTitle("Selecione uma pasta com músicas");
+		File pastaMusica = selecionarPasta.showDialog(primaryStage);
+
+		if (pastaMusica != null) {
+			DiretorioControle diretorio_controle = new DiretorioControle();
+			diretorio_controle.cadastrarDiretorioNoBanco(pastaMusica.getPath());
+			exibirMusicas(diretorio_controle);
+			diretorioAdicionado();
+		} else {
+			exibirAlerta("Erro", "Nenhuma pasta selecionada", Alert.AlertType.ERROR);
+		}
+	}
+
+	void exibirMusicas(DiretorioControle diretorio_controle) {
+		ObservableList<String> items = listaNomesMusicas.getItems();
+		ArrayList<Musica> listaMusicas = new ArrayList<>();
+
+		listaNomesMusicas.getItems().clear(); // limpando
+
+		for (Diretorio diretorio : diretorio_controle.listarDiretoriosDoBanco()) {
+			for (Musica musica : diretorio.getMusicas()) {
+				items.add(musica.getNome());
+				listaMusicas.add(musica);
+			}
+		}
+
+		listaNomesMusicas.setItems(items);
+
+		controller.setListaMusicas(listaMusicas);
 	}
 
 	@FXML
-	private void handleAlterarAssinatura(ActionEvent event) {
-		// Lógica para manipular o evento do MenuItem 'Alterar Assinatura'
+	void adicionarMusica(ActionEvent event) {
+		Stage primaryStage = new Stage();
+		FileChooser selecionarMusica = new FileChooser();
+		selecionarMusica.setTitle("Selecione uma música");
+		tocarMusica(selecionarMusica.showOpenDialog(primaryStage).toString());
 	}
 
 	@FXML
-	private void handleSair(ActionEvent event) {
-		// Lógica para manipular o evento do MenuItem 'Sair'
+	void adicionarPlaylist(ActionEvent event) {
+
 	}
 
 	@FXML
-	private void handleAdicionarMusica(ActionEvent event) {
-		// Lógica para manipular o evento do Button 'Adicionar Música'
+	void pausarTocarMusica(ActionEvent event) {
+		if (runing) {
+			this.botaoPlay.setText("Ⅱ");
+			this.mediaPlayer.pause();
+			runing = false;
+		} else {
+			this.botaoPlay.setText("►");
+			this.mediaPlayer.play();
+			runing = true;
+		}
 	}
 
 	@FXML
-	private void handleAdicionarDiretorio(ActionEvent event) {
-		// Lógica para manipular o evento do Button 'Adicionar Diretório'
+	void pularMusica(ActionEvent event) {
+
 	}
 
 	@FXML
-	private void handleAdicionarPlaylist(ActionEvent event) {
-		// Lógica para manipular o evento do Button 'Adicionar Playlist'
+	void voltarMusica(ActionEvent event) {
+
 	}
+
+	void carregarProgresso() {
+		mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+			@Override
+			public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+				double progress;
+				try {
+					progress = (mediaPlayer.getCurrentTime().toSeconds() / mediaPlayer.getTotalDuration().toSeconds());
+					progressoMusica.setProgress(progress);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	private void exibirAlerta(String titulo, String mensagem, Alert.AlertType tipo) {
+		Alert alerta = new Alert(tipo);
+		alerta.setTitle(titulo);
+		alerta.setContentText(mensagem);
+		alerta.show();
+	}
+
 }
--- src/application/br/ufrn/imd/controle/RegistroControle.java
+++ src/application/br/ufrn/imd/controle/RegistroControle.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
 import javafx.stage.Stage;
 
 public class RegistroControle {
-
 	@FXML
 	private TextField campoUsuario;
 
@@ -25,13 +27,11 @@ public class RegistroControle {
 	@FXML
 	private ChoiceBox<String> choiceBox;
 
-	// Método para inicializar o controlador após o carregamento do arquivo FXML
 	@FXML
 	public void initialize() {
-		// Aqui você pode adicionar opções à ChoiceBox
 		if (choiceBox != null) {
-			// Faça algo com o choiceBox
 			choiceBox.getItems().addAll("Comum", "VIP");
+			choiceBox.setValue("Comum");
 		}
 	}
 
@@ -50,13 +50,42 @@ public class RegistroControle {
 		loginStage.show();
 		RegistroControle registroControle = new RegistroControle();
 		loader.setController(registroControle);
-		
+
 		registroStage.close();
 	}
 
 	// Método para processar o registro
 	@FXML
 	public void processarRegistro() {
-		
+		String nome = campoNome.getText();
+		String login = campoUsuario.getText();
+		String senha = campoSenha.getText();
+		String tipo = choiceBox.getValue();
+
+		if (nome.isEmpty() || login.isEmpty() || senha.isEmpty()) {
+			exibirAlerta("Erro", "Por favor, preencha todos os campos!", Alert.AlertType.ERROR);
+		}
+
+		if (tipo.equals("VIP")) {
+			tipo = "V";
+		} else {
+			tipo = "C";
+		}
+
+		UsuarioControle usuario_controle = new UsuarioControle();
+		boolean cadastroRealizado = usuario_controle.cadastrarUsuarioNoBanco(nome, login, senha, tipo);
+
+		if (cadastroRealizado) {
+			exibirAlerta("Sucesso", "Registro Efetuado com Sucesso", Alert.AlertType.INFORMATION);
+		} else {
+			exibirAlerta("Falha", "Falha no Registro", Alert.AlertType.ERROR);
+		}
+	}
+
+	private void exibirAlerta(String titulo, String mensagem, Alert.AlertType tipo) {
+		Alert alerta = new Alert(tipo);
+		alerta.setTitle(titulo);
+		alerta.setContentText(mensagem);
+		alerta.show();
 	}
 }
