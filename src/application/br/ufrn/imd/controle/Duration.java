3d119c0b9b3c5ee0bba0cd5d44e31d08223ed4a3
Marcos Vinicius Noberto de Oliveira <112330160+marcosnoberto@users.noreply.github.com>
2023-12-05 23:08:58 -0300

    Muitas alterações

---
+++ src/application/br/ufrn/imd/controle/Duration.java
@@ -0,0 +1,5 @@
+package application.br.ufrn.imd.controle;
+
+public class Duration {
+
+}
--- src/application/br/ufrn/imd/visao/MediaPlayer.java
+++ src/application/br/ufrn/imd/visao/MediaPlayer.java
@@ -9,51 +9,52 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MediaPlayer extends Application {
-	
-	 @Override
-	    public void start(Stage primaryStage) {
-	        try {
-	            // Carrega o arquivo FXML
-	            FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/br/ufrn/imd/recursos/TelaLogin.fxml"));
-//	            loader.setController(new LoginControle());
-	            Parent root = loader.load();
 
-	            // Configura o palco (Stage)
-	            primaryStage.setTitle("Tela de Login");
-	            primaryStage.setScene(new Scene(root, 600, 400));
-	            primaryStage.show();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Carrega o arquivo FXML
+			FXMLLoader loader = new FXMLLoader(
+					getClass().getResource("/application/br/ufrn/imd/recursos/TelaLogin.fxml"));
+			// loader.setController(new LoginControle());
+			Parent root = loader.load();
 
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
+			// Configura o palco (Stage)
+			primaryStage.setTitle("Tela de Login");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-//    @Override
-//    public void start(Stage primaryStage) {
-//        TelaLogin telaLogin = new TelaLogin();
-//        telaLogin.start(primaryStage); // Chame o método start e passe o Stage
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-    
-//    public void start(Stage primaryStage) {
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	// @Override
+	// public void start(Stage primaryStage) {
+	// TelaLogin telaLogin = new TelaLogin();
+	// telaLogin.start(primaryStage); // Chame o método start e passe o Stage
+	// }
+	//
+	// public static void main(String[] args) {
+	// launch(args);
+	// }
+
+	// public void start(Stage primaryStage) {
+	// try {
+	// BorderPane root = new BorderPane();
+	// Scene scene = new Scene(root,400,400);
+	// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	// primaryStage.setScene(scene);
+	// primaryStage.show();
+	// } catch(Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	// public static void main(String[] args) {
+	// launch(args);
+	// }
 }
